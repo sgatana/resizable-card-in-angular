50ff3673ad61207e2a85cb6df58cdafa00fc4bce
sgatana <sgatana@gmail.com>
2025-05-03 17:38:30 +0300

    angular deep dive: implement a reusable resizable card component

---
+++ src/app/shared/resizable-card/resizable-card.component.ts
@@ -0,0 +1,79 @@
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-resizable-card',
+  templateUrl: './resizable-card.component.html',
+  styleUrls: ['./resizable-card.component.scss'],
+})
+export class ResizableCardComponent implements OnInit {
+  @ViewChild('card', { static: true }) cardElement!: ElementRef;
+  @ViewChild('dragHandle', { static: true }) dragHandleElement!: ElementRef;
+  @ViewChild('leftDragHandle', { static: true }) leftDragHandleElement!: ElementRef;
+  @ViewChild('rightDragHandle', { static: true }) rightDragHandleElement!: ElementRef;
+
+  defaultHeight = 300;
+  minHeight = 150;
+  maxHeight = window.innerHeight;
+  currentHeight = this.defaultHeight;
+  isDragging = false;
+  initialY!: number;
+  initialHeight!: number;
+
+  ngOnInit() {
+    this.currentHeight = this.defaultHeight;
+    this.setCardHeight();
+  }
+
+  setCardHeight() {
+    if (this.currentHeight) {
+      this.cardElement.nativeElement.style.height = `${this.currentHeight}px`;
+    }
+  }
+
+  @HostListener('mousedown', ['$event'])
+  onMouseDown(event: MouseEvent) {
+    const targets = [
+      this.cardElement.nativeElement,
+      this.dragHandleElement.nativeElement,
+      this.leftDragHandleElement.nativeElement
+    ];
+    if (targets.includes(event.target)) {
+      this.isDragging = true;
+      this.initialY = event.clientY;
+      this.initialHeight = this.currentHeight;
+      document.addEventListener('mousemove', this.onMouseMove);
+      document.addEventListener('mouseup', this.onMouseUp);
+      document.body.classList.add('resizing')
+    }
+  }
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    if (!this.isDragging) {
+      return;
+    }
+    console.log(event.clientY);
+    const deltaY = event.clientY - this.initialY;
+    let newHeight = this.initialHeight + deltaY;
+    // apply height constraints
+
+    newHeight = Math.max(this.minHeight, Math.min(newHeight, this.maxHeight));
+    this.currentHeight = newHeight;
+    this.setCardHeight();
+  }
+
+  @HostListener('document:mouseup')
+  onMouseUp() {
+    if (this.isDragging) {
+      this.isDragging = false;
+      document.removeEventListener('mousemove', this.onMouseMove);
+      document.removeEventListener('mouseup', this.onMouseUp);
+      document.body.classList.remove('resizing');
+    }
+  }
+}
